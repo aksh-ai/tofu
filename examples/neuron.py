a2674b76ee8fe862cc40086abaad38f4c18b2984
aksh-ai <akshayakki6899@gmail.com>
2020-04-14 22:22:34 +0530

    Updated files

--- examples/neuron.py
+++ examples/neuron.py
@@ -6,7 +6,7 @@ from tofu.layers import Linear
 
 inputs = np.random.uniform(low=0.1, high=5.0, size=(3, 3))
 
-linear1 = Linear(3, 3)
+linear1 = Linear(3, 1)
 
 print(f"Weights: \n{linear1.params[0]} \n\nBias: {linear1.params[1]:.5f}\n")
 
@@ -14,4 +14,4 @@ out = linear1(inputs)
 
 print(f"Output:\n{out}\n")		
 
-print(f"Argmax:\n{np.argmax(out[0])}\n")
+# print(f"Argmax:\n{np.argmax(out[0])}\n")
