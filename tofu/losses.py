c931bc8bbfa89da603fc492fc2e1901ba20153c5
aksh-ai <akshayakki6899@gmail.com>
2020-04-18 20:18:56 +0530

    Updated loss.py with functions

--- tofu/losses.py
+++ tofu/losses.py
@@ -10,6 +10,11 @@ def mse(y_true, y_pred):
 	loss = np.sum((y_true - y_pred)**2) / num_samples
 	return loss	
 
+def grad_mse(y_true, y_pred):
+	num_samples = y_true.size
+	loss = (2 * np.sum(np.abs(y_true - y_pred))) / num_samples
+	return loss
+
 def rmse(y_true, y_pred):
 	num_samples = y_true.size
 	loss = np.sum((y_true - y_pred)**2) / num_samples
@@ -34,4 +39,19 @@ def categorical_crossentropy(y_true, y_pred):
 	
 	mean_loss = 1.0 / len(y_true) * loss
 	
-	return -mean_loss	
+	return -mean_loss
+
+def softmax_crossentropy_with_logits(logits, true_logits):
+    nlogits = logits[np.arange(len(logits)), true_logits]
+    
+    entropy = - nlogits + np.log(np.sum(np.exp(logits), axis=-1))
+    
+    return entropy
+
+def grad_softmax_crossentropy_with_logits(logits, true_logits):
+    nlogits = np.zeros_like(logits)
+    nlogits[np.arange(len(logits)), true_logits] = 1
+    
+    softmax = np.exp(logits) / np.exp(logits).sum(axis=-1, keepdims=True)
+    
+    return (- nlogits + softmax) / logits.shape[0]
