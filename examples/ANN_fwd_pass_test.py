b6a4db469961e759cc6d669af34c5b255c46233a
aksh-ai <akshayakki6899@gmail.com>
2020-04-20 09:55:07 +0530

    Removed few lines

--- examples/ANN_fwd_pass_test.py
+++ examples/ANN_fwd_pass_test.py
@@ -12,7 +12,6 @@ class ANN:
 		self.dropout = Dropout(prob=0.4)
 		self.batch_norm = BatchNormalization()
 		self.fc2 = Linear(3, 1)
-		self.parameters = np.array([self.fc1.params, self.batch_norm.params, self.fc2.params])
 
 	def forward(self, X):
 		# performs single forward pass through the network through all layers as a chain
@@ -30,14 +29,10 @@ class ANN:
 
 model = ANN()
 
-inputs = np.random.uniform(low=0.1, high=5.0, size=(3, 3))
+inputs = np.random.uniform(low=0.1, high=50.0, size=(3, 3))
 
 # print(inputs,'\n')
 
 out = model.forward(inputs)
 
 print(f"Ouput value: {out[0, 0]:.5f}")
-
-print(f"Size of trainable network parameters: {model.parameters.size}")
-
-# 6 trainable params = fc1(W, b) + batch_norm(gamma, beta) + fc2(W, b)
