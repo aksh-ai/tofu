6bbe3161f0dd807566c94502602e12a3ed2455b7
aksh-ai <akshayakki6899@gmail.com>
2020-04-20 09:57:20 +0530

    Updated script with access modifiers and new functions

--- tofu/nn.py
+++ tofu/nn.py
@@ -8,12 +8,12 @@ class Model:
 
 		try:
 			if loss == 'crossentropy':
-				self.loss = softmax_crossentropy_with_logits
-				self.grad_loss = grad_softmax_crossentropy_with_logits
+				self.__loss = softmax_crossentropy_with_logits
+				self.__grad_loss = grad_softmax_crossentropy_with_logits
 
 			elif loss == 'mse':
-				self.loss = mse
-				self.grad_loss = grad_mse
+				self.__loss = mse
+				self.__grad_loss = grad_mse
 
 			else:
 				print("Invalid loss function defined")
@@ -25,15 +25,18 @@ class Model:
 		self.layers.append(layer)
 
 	def __forward(self, X):
-		activations = []
-		inputs = X
+		try:
+			activations = []
+			inputs = X
 
-		for layer in self.layers:
-			activations.append(layer(inputs))
-			inputs = activations[-1]
-		
-		assert len(activations) == len(self.layers)
-		return activations
+			for layer in self.layers:
+				activations.append(layer(inputs))
+				inputs = activations[-1]
+			
+			return activations
+
+		except Exception as e:
+			print(f"Error in forwarding through layers\n\n{e}\n")	
 
 	def predict(self, X):
 		logits = self.__forward(X)[-1]
@@ -41,11 +44,11 @@ class Model:
 
 	def __train(self, X , y, learning_rate):
 		layer_activations = self.__forward(X)
-		layer_inputs = [X]+layer_activations  
+		layer_inputs = [X] + layer_activations  
 		logits = layer_activations[-1]
 		
-		loss = self.loss(logits, y)
-		loss_grad = self.grad_loss(logits, y)
+		loss = self.__loss(logits, y)
+		loss_grad = self.__grad_loss(logits, y)
 		
 		for layer_index in range(len(self.layers))[::-1]:
 			layer = self.layers[layer_index]
@@ -55,26 +58,30 @@ class Model:
 		return np.mean(loss)
 
 	def __create_batches(self, inputs, targets, batch_size, shuffle=False):
-		assert len(inputs) == len(targets)
-		
-		if shuffle:
-			indices = np.random.permutation(len(inputs))
-		
-		for start_idx in range(0, len(inputs) - batch_size + 1, batch_size):
+		try:
 			if shuffle:
-				excerpt = indices[start_idx:start_idx + batch_size]
+				indices = np.random.permutation(len(inputs))
 			
-			else:
-				excerpt = slice(start_idx, start_idx + batch_size)
-			
-			yield inputs[excerpt], targets[excerpt]
+			for start_idx in range(0, len(inputs) - batch_size + 1, batch_size):
+				if shuffle:
+					excerpt = indices[start_idx:start_idx + batch_size]
+				
+				else:
+					excerpt = slice(start_idx, start_idx + batch_size)
+				
+				yield inputs[excerpt], targets[excerpt]
+
+		except Exception as e:
+			print(f"Error in creating batches\n\n{e}\n")		
 
 	def validate(self, X, y):
 		logits = self.__forward(X)[-1]
-		loss = self.loss(logits, y)
+		loss = self.__loss(logits, y)
 		return np.mean(loss)
 
 	def fit(self, X_train, y_train, epochs=30, batch_size=32, validation_data=(), learning_rate=0.1, verbose=5):        
+		print(f"{int(len(X_train)/batch_size)} batch of samples per epoch | {len(X_train)} number of samples in total")
+
 		train_acc = []
 		train_loss = []
 		val_acc = [] 
@@ -87,7 +94,7 @@ class Model:
 		for epoch in range(epochs):
 			start_time = time.time()
 
-			for x_batch, y_batch in self.__create_batches(X_train, y_train, batch_size=32, shuffle=True):
+			for x_batch, y_batch in self.__create_batches(X_train, y_train, batch_size=batch_size, shuffle=True):
 				t_loss = self.__train(x_batch, y_batch, learning_rate)
 			
 			train_acc.append(np.mean(self.predict(X_train) == y_train))
@@ -98,8 +105,8 @@ class Model:
 			end_time = time.time() - start_time
 
 			if epoch==0 or epoch==(epochs-1) or (epoch % verbose == 0):
-				print(f"Epoch: {epoch+1}")
-				print(f"accuracy: {train_acc[-1]:.4f}  loss: {train_loss[-1]:.4f}  val_accuracy: {val_acc[-1]:.4f}  val_loss: {val_loss[-1]:.4f}  time: {(end_time/60):.2f} mins")
+				print(f"Epoch {epoch+1}")
+				print(f"accuracy: {train_acc[-1]:.4f} - loss: {train_loss[-1]:.4f} - val_accuracy: {val_acc[-1]:.4f} - val_loss: {val_loss[-1]:.4f} - time: {(end_time/60):.2f} mins")
 
 		print(f"\nTotal Training Duration: {(time.time() - tot_start) / 60:.2f} mins")
 		return {'accuracy': train_acc, 'val_accuracy': val_acc, 'loss': train_loss, 'val_loss': val_loss}
