18451466a2731a58bbb8f0c3c2143c656e45b50e
aksh-ai <akshayakki6899@gmail.com>
2020-04-20 09:56:27 +0530

    Added new layers and updated existing with backward pass fucntions

--- tofu/layers.py
+++ tofu/layers.py
@@ -3,13 +3,12 @@ import numpy as np
 class Linear:
 	def __init__(self, shape_1, shape_2, name=None):
 		self.name = name
-		self.shape_1 = shape_1
-		self.shape_2 = shape_2
+		self.__shape_1 = shape_1
+		self.__shape_2 = shape_2
+		self.__lim = np.sqrt(6/(0.5 + shape_1 + shape_2))
 		
-		self.weights = np.random.normal(loc=0.0, scale=np.sqrt(6/(shape_1 + shape_2)), size=(shape_1, shape_2))
-		self.bias = np.random.normal(loc=0.0, scale=np.sqrt(6/(shape_1 + shape_2)), size=shape_2)
-		
-		self.params = [self.weights, self.bias]
+		self.weights = np.random.uniform(low= -self.__lim, high= self.__lim, size=(self.__shape_1, self.__shape_2))
+		self.bias = np.zeros(shape=self.__shape_2)
 
 	def __call__(self, inputs):
 		return np.dot(inputs, self.weights) + self.bias
@@ -29,63 +28,82 @@ class Dropout:
 	def __init__(self, prob=0.5, name=None):
 		self.name = name
 		self.prob = prob
+		self.__indices = None
 
 	def __call__(self, inputs):
 		dropped = inputs.flatten()
 		num_drop = self.prob * inputs.size
-		indices = np.random.choice(len(dropped), size=int(num_drop))
+		self.__indices = np.random.choice(len(dropped), size=int(num_drop))
 		
-		for i in indices:
-			dropped[i] = np.zeros(1, dtype=np.float32)[0]
+		for i in self.__indices:
+			dropped[i] = 0 
+			
+		dropped = dropped * (1/(1 - self.prob))
 
 		return dropped.reshape(inputs.shape)
 
 	def backward(self, inputs, grad_out, learning_rate):
-		return grad_out	
+		for i in self.__indices:
+			grad_out[i] = 0 
+			
+		grad_out = grad_out * (1 / (1 - self.prob))
+		
+		return grad_out
 
 class BatchNormalization:
-	def __init__(self, momentum=0.99, epsilon=1e-5, axis=-1, training=True, name=None):
+	def __init__(self, momentum=0.99, epsilon=1e-8, axis=0, training=True, name=None):
 		self.name = name
 
 		self.momentum = momentum
-		self.gamma = np.random.normal(loc=0.0, scale=0.02, size=1)[0]
-		self.beta = np.random.normal(loc=0.0, scale=0.02, size=1)[0]
+		
+		self.gamma = 1.0
+		self.beta = 0.0
 		self.epsilon = epsilon
 		
 		self.axis = axis
 		
 		self.mu = None
-		self.sigma = None
+		self.var = None
 
 		self.training = True
 
-		self.params = np.array([self.gamma, self.beta])
+	def __call__(self, inputs):
+		if self.training == False:
+			self.X_norm = (inputs - self.mu) / np.sqrt(self.var + self.epsilon)
+			return self.gamma * self.X_norm + self.beta
+		
+		self.mu = np.mean(inputs, axis=self.axis)
+		self.var = np.var(inputs, axis=self.axis)
 
-	def __call__(self, X):
-		if self.training==True:
-			self.mu = X.mean(axis=self.axis)
-			self.sigma = (X - self.mu)**2 / X.size
+		self.X_norm = (inputs - self.mu) / (np.sqrt(self.var + self.epsilon) ** 0.5)
+		
+		out = self.gamma * self.X_norm + self.beta
 
-		else:
-			self.mu = self.mu * self.momentum + self.mu * (1 - self.momentum)
-			self.sigma = self.sigma * self.momentum + self.sigma * (1 - self.momentum)
+		self.mu = self.mu * self.momentum + self.mu * (1 - self.momentum)
+		self.var = self.var * self.momentum + self.var * (1 - self.momentum)
 
-		X_hat = (X - self.mu) / np.sqrt(self.sigma + self.epsilon)
-
-		y =	self.gamma * X_hat + self.beta
-
-		return y
+		return out
 
 	def backward(self, inputs, grad_out, learning_rate):
-		grad_in = self.gamma * inputs + self.beta
+		N = inputs.shape[0]
 
-		d_gamma = (grad_out * self.gamma)
-		d_beta = (grad_out.mean(axis=0) * inputs.shape[0])
+		X_mu = inputs - self.mu
+		std_inv = 1. / np.sqrt(self.var + self.epsilon)
 
-		self.gamma -= learning_rate * d_gamma
-		self.beta -= learning_rate * d_beta
+		dX_norm = grad_out * self.gamma
+		
+		d_var = np.sum(dX_norm * X_mu, axis=self.axis) * -.5 * std_inv**3
+		d_mu = np.sum(dX_norm * -std_inv, axis=self.axis) + d_var * np.mean(-2. * X_mu, axis=self.axis)
 
-		return grad_in
+		dX = (dX_norm * std_inv) + (d_var * 2 * X_mu / N) + (d_mu / N)
+		
+		dgamma = np.sum(grad_out * self.X_norm, axis=0)
+		dbeta = np.sum(grad_out, axis=0)
+
+		self.gamma -= learning_rate * dgamma
+		self.beta -= learning_rate * dbeta
+
+		return dX 
 
 class ReLU:
 	def __init__(self):
@@ -106,7 +124,8 @@ class LeakyReLU:
 		return np.maximum(inputs, inputs * self.alpha)
 
 	def backward(self, inputs, grad_out, learning_rate):
-		out = 1 if inputs.any() > 0 else self.alpha
+		out = np.ones_like(inputs)
+		out[inputs < 0] = self.alpha
 		return grad_out * out
 
 class ELU:
@@ -114,10 +133,10 @@ class ELU:
 		self.alpha = alpha
 
 	def __call__(self, inputs):
-		return np.maximum(inputs, self.alpha*(np.exp(inputs) - 1))
+		return np.where(inputs<=0, self.alpha * (np.exp(inputs) - 1), inputs)
 
 	def backward(self, inputs, grad_out, learning_rate):
-		out = 1 if inputs.any() > 0 else self.alpha * np.exp(inputs) #+ inf
+		out = np.where(inputs<=0, self.alpha * np.exp(inputs), 0)
 		return grad_out * out
 
 class TanH:
