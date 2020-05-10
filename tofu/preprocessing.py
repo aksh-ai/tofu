8951b20c4fe3d8e72dcaef5b4f7786d77058a63f
aksh-ai <akshayakki6899@gmail.com>
2020-05-10 19:34:44 +0530

    Updated preprocessing script

--- tofu/preprocessing.py
+++ tofu/preprocessing.py
@@ -53,9 +53,6 @@ def train_test_split(X, y, train_size=None, test_size=None, random_seed=None):
 		print("Number of samples and labels don't match")
 		return
 
-	if random_seed:
-		random.seed(random_seed)
-
 	if train_size:
 		test_size = round((1.0 - train_size), 1)
 		
@@ -66,4 +63,29 @@ def train_test_split(X, y, train_size=None, test_size=None, random_seed=None):
 	y_test = y[0:test_size]
 	y_train = y[test_size:len(y)]
 
-	return X_train, X_test, y_train, y_test			
+	X_train, y_train = shuffle(X_train, y_train, random_seed)
+	X_test, y_test = shuffle(X_test, y_test, random_seed)
+
+	return np.array(X_train), np.array(X_test), np.array(y_train), np.array(y_test)
+
+def shuffle(a=None, b=None, random_seed=None):
+	if random_seed:
+		random.seed(random_seed)
+
+	if a is not None and b is not None:
+		shuffler = list(zip(a, b))
+		random.shuffle(shuffler)
+		a, b = zip(*shuffler)
+		return np.array(a), np.array(b)
+
+	elif a is not None:
+		random.shuffle(a)
+		return np.array(a)
+
+	elif b is not None:
+		random.shuffle(b)
+		return np.array(b)
+
+	else:
+		print('Array(s) are empty')
+		return None
