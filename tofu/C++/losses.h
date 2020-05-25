4565ca2a0f4e68e94ea4ad53635029366f9ece33
aksh-ai <akshayakki6899@gmail.com>
2020-05-25 22:27:36 +0530

    Added loss fucntions as header file

---
+++ tofu/C++/losses.h
@@ -0,0 +1,22 @@
+#include "tensor.h"
+
+Tensor *mae(Tensor *y_pred, Tensor *y_true)
+{
+    Tensor *loss = (Tensor *) malloc(sizeof(Tensor));
+    loss = matsub(y_pred, y_true);
+    return loss;
+}
+
+Tensor *mse(Tensor *y_pred, Tensor *y_true)
+{
+    Tensor *loss = (Tensor *) malloc(sizeof(Tensor));
+    loss = square(matsub(y_pred, y_true));
+    return loss;
+}
+
+Tensor *rmse(Tensor *y_pred, Tensor *y_true)
+{
+    Tensor *loss = (Tensor *) malloc(sizeof(Tensor));
+    loss = sqrt(square(matsub(y_pred, y_true)));
+    return loss;
+}
