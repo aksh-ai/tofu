6dd4a4a919d4865344a35cb023d0eabc619fe043
aksh-ai <akshayakki6899@gmail.com>
2020-07-28 13:53:29 +0530

    Updated test files

--- tofu/C++/test.cpp
+++ tofu/C++/test.cpp
@@ -3,45 +3,52 @@
 #include <random>
 #include "layers.h"
 
+class Net
+{
+  int in_feat;
+  int classes;
+
+  public:
+    Net(int in_feat=16, int classes=1)
+    {
+      this->in_feat = in_feat;
+      this->classes = classes;
+    }
+    
+    Tensor *forward(Tensor *X)
+    {
+      Linear fc1(this->in_feat, 32);
+      ELU a1(0.2);
+      Linear fc2(32, 64);
+      ELU a2(0.2);
+      Linear fc3(64, this->classes);
+      Sigmoid a3; 
+      
+      Tensor *out = fc1.forward(X);
+      out = a1.forward(out);
+      out = fc2.forward(out);
+      out = a2.forward(out);
+      out = fc3.forward(out);
+      out = a3.forward(out);
+
+      return out;
+    }
+};
+
 int main()
 {
-    Linear fc1(5, 2);
+    Net net(16, 1);
 
     Tensor *a = (Tensor *) malloc(sizeof(Tensor));
-    Tensor *res = (Tensor *) malloc(sizeof(Tensor));
 
-    a->shape[0] = 2, a->shape[1] = 5;
+    a->shape[0] = 1, a->shape[1] = 16;
 
-    a = xavier_initializer(a, 5.0);
+    a = xavier_initializer(a, -0.350);
 
-    printf("\nLinear Layer => Weights: \n\n");
-
-    for(size_t i=0; i < fc1.weights->shape[0]; i++)
-    {
-        for (size_t j = 0; j < fc1.weights->shape[1]; j++)
-        {
-           printf("%Lf  ", fc1.weights->tensor[i][j]);
-        }
-        
-        printf("\n");
-    }
-
-    printf("\nLinear Layer => Bias: \n\n");
-
-    for(size_t i=0; i < fc1.bias->shape[0]; i++)
-    {
-        for (size_t j = 0; j < fc1.bias->shape[1]; j++)
-        {
-           printf("%Lf  ", fc1.bias->tensor[i][j]);
-        }
-        
-        printf("\n");
-    }
+    Tensor *res = net.forward(a);
 
     printf("\nForward Propagation: \n\n");
-
-    res = fc1.forward(a);
-
+  
     for(size_t i=0; i < res->shape[0]; i++)
     {
         for (size_t j = 0; j < res->shape[1]; j++)
